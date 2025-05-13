7e15822a194e4d5811c28f71ab8a88c9efcd76a2
MateusCforine <mateuscf938@gmail.com>
2025-05-13 19:08:50 -0300

    Códigos corrigidos

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector("#btn1").addEventListener('click', function () {
         const titulo = document.querySelector("#titulo");
-        titulo.innerHTML = "Tarefa DOM!";
+        titulo.innerHTML = "Título Alterado!";
         titulo.classList.toggle("ativo");
-        document.querySelector("#som1").play();
     });
 
     document.querySelector("#btn2").addEventListener('click', function () {
         const img = document.querySelector("#imagem");
-        img.src = "cavalo.jpeg";
+        img.src = img.src.includes("cachorro.jpeg") ? "cavalo.jpeg" : "cachorro.jpeg";
         img.style.width = "200px";
-        document.querySelector("#som2").play();
     });
 
     document.querySelector("#btn3").addEventListener('click', function () {
         const texto1 = document.querySelector("#texto1");
-        texto1.innerHTML = "Olá Mundo!";
+        texto1.innerHTML = "Texto 1 Modificado!";
         texto1.classList.toggle("ativo");
-        document.querySelector("#som3").play();
     });
 
     const botoesTexto2 = ["btn4", "btn5", "btn6"];
+    const frases = {
+        "btn4": "Você apertou o Botão 4",
+        "btn5": "Agora apertou o Botão 5.",
+        "btn6": "Finalmente apertou o Botão 6."
+    };
+
     for (let id of botoesTexto2) {
         document.querySelector("#" + id).addEventListener('click', function () {
-            document.querySelector("#texto2").innerHTML = this.innerHTML;
+            document.querySelector("#texto2").innerHTML = frases[id];
         });
     }
 });
